f3b045b00737a100b6d98cf863130c45b1e01892
stano <>
2025-01-14 18:33:47 +0200

    Replace the hardcoded command with lsp settings

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
-use zed_extension_api as zed;
+use zed_extension_api::{self as zed};
 
-struct MojoLspExtension {
-    // ... state
+// The name used to configure the lsp server in the Zed settings
+const LSP_SERVER_ID: &str = "mojo-lsp";
+
+struct MojoExtension {
+    command: String,
+    args: Vec<String>,
 }
 
-impl zed::Extension for MojoLspExtension {
+impl zed::Extension for MojoExtension {
     fn new() -> Self {
-        Self {}
+        Self {
+            command: "magic".to_string(),
+            args: vec!["run".to_string(), "mojo-lsp-server".to_string()],
+        }
     }
 
     fn language_server_command(
@@ -14,9 +21,26 @@ impl zed::Extension for MojoLspExtension {
         _language_server_id: &zed::LanguageServerId,
         _worktree: &zed::Worktree,
     ) -> zed::Result<zed::Command> {
+        let settings = zed::settings::LspSettings::for_worktree(LSP_SERVER_ID, _worktree);
+
+        if let Ok(lsp_settings) = settings {
+            if let Some(binary) = lsp_settings.binary {
+                let env: Vec<(String, String)> = match binary.env {
+                    Some(m) => m.into_iter().collect(),
+                    None => Vec::new(),
+                };
+
+                return Ok(zed::Command {
+                    command: binary.path.unwrap_or(self.command.clone()),
+                    args: binary.arguments.unwrap_or(self.args.clone()),
+                    env,
+                });
+            }
+        }
+
         Ok(zed::Command {
-            command: "magic".to_string(),
-            args: vec!["run".to_string(), "mojo-lsp-server".to_string()],
+            command: self.command.clone(),
+            args: self.args.clone(),
             env: vec![],
         })
     }
@@ -26,7 +50,7 @@ impl zed::Extension for MojoLspExtension {
         _language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> zed::Result<Option<zed::serde_json::Value>> {
-        let settings = zed::settings::LspSettings::for_worktree("mojo-lsp", worktree)
+        let settings = zed::settings::LspSettings::for_worktree(LSP_SERVER_ID, worktree)
             .ok()
             .and_then(|lsp_settings| lsp_settings.settings.clone())
             .unwrap_or_default();
@@ -34,4 +58,4 @@ impl zed::Extension for MojoLspExtension {
     }
 }
 
-zed::register_extension!(MojoLspExtension);
+zed::register_extension!(MojoExtension);
